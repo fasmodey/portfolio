b3f4b84e018374911c0bb20bc4e80e1af00d14fe
fasmodey <khudanalexandr@gmail.com>
2020-06-12 23:08:33 +0300

    no message

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const frame = document.querySelector('.projects-frame');
+const showFrame = document.querySelector('.show-frame');
+const hideFrame = document.querySelector('.hide-frame');
+const form = document.querySelector('form');
+const projects = {
+    'scss': 'example/scss-preprocessor/index.html',
+    'life-blog': 'example/life-blog/index.html',
+    'bootstrap': 'example/bootstrap-layout/index.html',
+    'travel-blog': 'example/travel-blog/index.html'
+}
+
+let showProject;
+
+form.addEventListener('click', event => {
+    if (event.target.getAttribute('name') === 'preview') {
+        showProject = event.target.value;
+        console.log(showProject);
+    }
+});
+
+showFrame.addEventListener('click', () => {
+
+    if (showProject) {
+        if (frame.classList.contains('hidden')) {
+            frame.classList.remove('hidden');
+            frame.classList.add('active');
+        }
+        frame.src = projects[showProject];
+    }
+});
+
+hideFrame.addEventListener('click', () => {
+    if (frame.classList.contains('active')) {
+        frame.classList.remove('active');
+        frame.classList.add('hidden');
+    }
+});
